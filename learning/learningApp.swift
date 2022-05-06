56aac08c9e7f6b6a122dd41f44eecaa22bf1f0a5
daniel jermaine <danieljermaine2@gmail.com>
2022-05-06 12:15:41 +0100

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Apple on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
